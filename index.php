c67af980d70732c73cd6b051b33689144770b457
il-tanito <contact@tanito.me>
2020-01-15 02:21:36 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<!-- links, scripts y titulo -->
+<title> Email Checker by il tanito</title>
+<script src="https://kit.fontawesome.com/c57e637d22.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Armata">>
+<link rel="stylesheet" href="style.css">
+
+<!-- body y actions -->
+<body>
+
+<div class="title">Email Checker</div>
+<form action="check.php" method="post">
+<div class="inner">Email Address:</div> 
+<input type="email" name="email" />
+<br>
+<input  value="Check" type="submit" />
+</form>
+
+</body>
+
+<!-- fin -->
+</html>
